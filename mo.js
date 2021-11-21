908c8e48c777da2c2d31dac6b7db7b926c42972f
FumiyaOchiai <80737623+FumiyaOchiai@users.noreply.github.com>
2021-11-21 15:40:28 +0900

    Add files via upload

---
+++ mo.js
@@ -0,0 +1,77 @@
+//マウスストーカー用のdivを取得
+const stalker = document.getElementById('stalker');
+
+//aタグにホバー中かどうかの判別フラグ
+let hovFlag = false;
+
+//マウスに追従させる処理 （リンクに吸い付いてる時は除外する）
+document.addEventListener('mousemove', function (e) {
+    if (!hovFlag) {
+    stalker.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+    }
+});
+
+//リンクへ吸い付く処理
+const linkElem = document.querySelectorAll('a:not(.no_stick_)');
+for (let i = 0; i < linkElem.length; i++) {
+    //マウスホバー時
+    linkElem[i].addEventListener('mouseover', function (e) {
+        hovFlag = true;
+
+        //マウスストーカーにクラスをつける
+        stalker.classList.add('hov_');
+
+        //マウスストーカーの位置をリンクの中心に固定
+        let rect = e.target.getBoundingClientRect();
+        let posX = rect.left + (rect.width / 2);
+        let posY = rect.top + (rect.height / 2);
+
+        stalker.style.transform = 'translate(' + posX + 'px, ' + posY + 'px)';
+
+    });
+    //マウスホバー解除時
+    linkElem[i].addEventListener('mouseout', function (e) {
+        hovFlag = false;
+        stalker.classList.remove('hov_');
+    });
+}
+
+// // swiper----------------------------
+// const swiper = new Swiper('.swiper-container', {
+//     slidesPerView: 6,
+//         spaceBetween: 20,
+//         // slidesPerGroup: 3,
+//         loop: true,
+//         // loopFillGroupWithBlank: true,
+//         //  effect:'coverflow',
+
+//         autoplay:{
+//             delay:2500,
+//         },
+//         pagination: {
+//             el: '.swiper-pagination',
+//             clickable: true,
+//         },
+        
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+//   });
+
+// // メニューオープン
+//   (function ($) {
+//     $(function () {
+//         $('#nav-toggle').on('click', function () {
+//             $('body').toggleClass('open');
+//         });
+//     });
+// })(jQuery);
+
+
